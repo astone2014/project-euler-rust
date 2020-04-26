3329f6350a585518ffcb4e30ad6fcab76f17e125
Austen Stone <miltonlaxer@gmail.com>
2020-04-26 14:44:25 -0400

    Problem 8 done

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ fn main() {
     // println!("Problem 5: {}", problem5());
     // println!("Problem 6: {}", problem6());
     // println!("Problem 7: {}", problem7());
-    println!("Problem 8: {}", problem8());
+    // println!("Problem 8: {}", problem8());
+    println!("Problem 9: {}", problem9());
 }
 
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -72,28 +73,6 @@ fn problem7() -> i64 {
 }
 
 // The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
-//
-// 73167176531330624919225119674426574742355349194934
-// 96983520312774506326239578318016984801869478851843
-// 85861560789112949495459501737958331952853208805511
-// 12540698747158523863050715693290963295227443043557
-// 66896648950445244523161731856403098711121722383113
-// 62229893423380308135336276614282806444486645238749
-// 30358907296290491560440772390713810515859307960866
-// 70172427121883998797908792274921901699720888093776
-// 65727333001053367881220235421809751254540594752243
-// 52584907711670556013604839586446706324415722155397
-// 53697817977846174064955149290862569321978468622482
-// 83972241375657056057490261407972968652414535100474
-// 82166370484403199890008895243450658541227588666881
-// 16427171479924442928230863465674813919123162824586
-// 17866458359124566529476545682848912883142607690042
-// 24219022671055626321111109370544217506941658960408
-// 07198403850962455444362981230987879927244284909188
-// 84580156166097919133875499200524063689912560717606
-// 05886116467109405077541002256983155200055935729725
-// 71636269561882670428252483600823257530420752963450
-//
 // Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 fn problem8() -> i64 {
     let digits = "73167176531330624919225119674426574742355349194934\
@@ -116,11 +95,47 @@ fn problem8() -> i64 {
     84580156166097919133875499200524063689912560717606\
     05886116467109405077541002256983155200055935729725\
     71636269561882670428252483600823257530420752963450";
-    let bytes = digits.as_bytes();
-    for d in bytes.iter() {
-        println!("{}!", d);
+    return get_largest_adjacent_product(digits, 13);
+}
+
+// A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+//
+// a^2 + b^2 = c^2
+// For example, (3^2 + 4^2) = (9 + 16) = 25 = 5^2.
+//
+// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+// Find the product abc.
+fn problem9() -> i64 {
+    for b in 0..999 {
+        for a in 0..b {
+            let c = ((a + b) as f64).sqrt();
+            // println!("{} + {} + {} = {}", a, b, c, 1000);
+            if (a + b + c as i32) == 1000 {
+                println!("{} + {} = {}, product = {}", a, b, c, a * b * c as i32);
+            }
+        }
     }
     return 0;
+}
+
+fn get_largest_adjacent_product(string: &str, num_adjacent: usize) -> i64 {
+    let chars = string.chars();
+    let bytes = string.as_bytes();
+    let mut greatest: i64 = 0;
+    let length = string.len();
+    for i in 0..(length - num_adjacent) {
+        let mut numbers: Vec<i64> = vec![];
+        for j in 0..num_adjacent {
+            let c = bytes[i + j] as char;
+            let d = c.to_digit(10).unwrap();
+            numbers.push(d as i64);
+        }
+        let product: i64 = numbers.iter().product();
+        if product > greatest {
+            greatest = product;
+        }
+    }
+    return greatest;
 }
 
 fn get_nth_prime_number(n: i64) -> i64 {
